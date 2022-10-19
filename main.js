d7102cc54f35b46c9d0f6b153da05893526ee721
Nutty-nuts <vegagerard1130@gmail.com>
2022-10-19 11:32:04 +0800

    added node & BST classes

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Working");
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
