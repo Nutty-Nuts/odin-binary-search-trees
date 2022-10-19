e59a7f7737723b740e51597f7a222b1fa9750c1d
Nutty-nuts <vegagerard1130@gmail.com>
2022-10-19 12:17:49 +0800

    added insert node

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ class Node {
 }
 
 /*
+ * buildTree, takes an array of data and creates a balanced BinarySearchTree
+ */
+
+/*
  * BinarySearchTree Data Methods
  *
  * insert, insert a node based on value
@@ -39,4 +43,42 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(data) {
+        const node = this.root;
+
+        if (node === null) {
+            this.root = new Node(data);
+            return;
+        }
+
+        function search(node) {
+            if (data < node.data) {
+                if (node.left === null) {
+                    node.left = new Node(data);
+                    return;
+                } else if (node.left !== null) return search(node.left);
+            } else if (data > node.data) {
+                if (node.right === null) {
+                    node.right = new Node(data);
+                    return;
+                } else if (node.right !== null) return search(node.right);
+            } else {
+                return null;
+            }
+        }
+        return search(node);
+    }
 }
+
+let tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(13);
+tree.insert(17);
+
+tree.print();
+
+console.log(tree);
