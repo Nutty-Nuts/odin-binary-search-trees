398929925c4fc268b0264d2587922b8c9a1478db
Nutty-nuts <vegagerard1130@gmail.com>
2022-10-19 11:35:16 +0800

    added todo comments

--- main.js
+++ main.js
@@ -6,6 +6,35 @@ class Node {
     }
 }
 
+/*
+ * BinarySearchTree Data Methods
+ *
+ * insert, insert a node based on value
+ * delete, delete a node based on value
+ * find, find a node based on value
+ */
+
+/*
+ * BinarySearchTree Search Traversal Methods
+ *
+ * levelOrder, travesrses via breadth-first levelOrder
+ * inorder, preorder, postorder, traverses via respective depth-first order
+ */
+
+/*
+ * BinarySearchTree Length Methods
+ *
+ * height, returns the height of a node
+ * depth, returns the depth of a node
+ */
+
+/*
+ * BinarySearchTree Balancing Methods
+ *
+ * isBalanced, checks if BinarySearchTree is isBalanced
+ * reBalance, reBalance a unbalanced BinarySearchTree
+ */
+
 class BinarySearchTree {
     constructor() {
         this.root = null;
