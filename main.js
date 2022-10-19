af2f216c656fea63cc55853e9fb6eaf1fc89876f
Nutty-nuts <vegagerard1130@gmail.com>
2022-10-19 12:29:50 +0800

    added build tree

--- main.js
+++ main.js
@@ -44,6 +44,56 @@ class BinarySearchTree {
         this.root = null;
     }
 
+    print() {
+        const node = this.root;
+        function prettyPrint(node, prefix = "", isLeft = true) {
+            if (node.right !== null) {
+                prettyPrint(
+                    node.right,
+                    `${prefix}${isLeft ? "│   " : "    "}`,
+                    false
+                );
+            }
+            console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+            if (node.left !== null) {
+                prettyPrint(
+                    node.left,
+                    `${prefix}${isLeft ? "    " : "│   "}`,
+                    true
+                );
+            }
+        }
+        prettyPrint(node);
+    }
+
+    buildTree(array) {
+        for (let data of array) {
+            const node = this.root;
+
+            if (node === null) {
+                this.root = new Node(data);
+                continue;
+            }
+
+            function search(node) {
+                if (data < node.data) {
+                    if (node.left === null) {
+                        node.left = new Node(data);
+                        return;
+                    } else if (node.left !== null) return search(node.left);
+                } else if (data > node.data) {
+                    if (node.right === null) {
+                        node.right = new Node(data);
+                        return;
+                    } else if (node.right !== null) return search(node.right);
+                } else {
+                    return null;
+                }
+            }
+            search(node);
+        }
+    }
+
     insert(data) {
         const node = this.root;
 
@@ -70,15 +120,10 @@ class BinarySearchTree {
         return search(node);
     }
 }
-
+let array = [10, 5, 15, 3, 7, 13, 17];
 let tree = new BinarySearchTree();
 
-tree.insert(10);
-tree.insert(5);
-tree.insert(15);
-tree.insert(13);
-tree.insert(17);
-
+tree.buildTree(array);
 tree.print();
 
 console.log(tree);
